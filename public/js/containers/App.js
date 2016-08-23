147d1bbd5526d6d4beabb477af64a8607f85f89b
RangelZZZ <18792561236@163.com>
2016-08-23 14:56:06 +0800

    Add Css for Header

--- public/js/containers/App.js
+++ public/js/containers/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from "react";
+import Register from '../components/Register';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {this.props.children}
+        <Register/>
       </div>
     );
   }
